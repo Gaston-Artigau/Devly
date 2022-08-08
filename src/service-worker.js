f76593b569b9d910c8495d9acdd955b3cdc346ba
Gaston-Artigau <gastonartigau@gmail.com>
2022-08-07 21:17:09 -0300

    Update

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,7 +6,7 @@ import { StaleWhileRevalidate } from 'workbox-strategies';
 
 clientsClaim();
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(window.self.__WB_MANIFEST);
 
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
