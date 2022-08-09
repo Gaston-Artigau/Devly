9dec67cc3e30a1b8a4eae31f0345fc0878d0bdf7
Gaston-Artigau <gastonartigau@gmail.com>
2022-08-09 11:51:52 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import AppRouter from './routes/AppRouter';import ReactGA from 'react-ga';
+import AppRouter from './routes/AppRouter';
+import ReactGA from 'react-ga';
 
 function App() {
   const TRACKING_ID = "G-MS3CLZ3JKN"; // OUR_TRACKING_ID
